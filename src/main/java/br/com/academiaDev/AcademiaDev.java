4d0469c0f94b0b433422620ace596a009b0e7d0c
Raul Melo <raul.m@aluno.ifsp.edu.br>
2025-11-26 14:41:43 -0300

    first commit

---
+++ src/main/java/br/com/academiaDev/AcademiaDev.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.academiaDev;
+
+/**
+ *
+ * @author raul.farias
+ */
+public class AcademiaDev {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
